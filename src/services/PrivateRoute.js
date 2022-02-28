48e7b8c17c940aa511bea879b37a88c9ee939c3e
Paulo Martins <pfmpaulomartins@gmail.com>
2022-02-28 13:33:21 -0300

    refactor private route component

--- src/services/PrivateRoute.js
+++ src/services/PrivateRoute.js
@@ -3,23 +3,10 @@ import { Navigate } from 'react-router-dom';
 import { useAuth } from './Auth'
 
 const PrivateRoute = ({ children }) => {
-    console.log(children)
-    const auth = useAuth();
-    console.log('private', auth)
-    return auth ? children : <Navigate to="/login" />;
-};
-
-const ProtectedRoute = ({ children }) => {
-    const { token } = useAuth();
-
-    if (!token) {
-        return <Navigate to="/login" replace />;
-    }
-
-    return children;
+    const { signed } = useAuth();
+    return signed ? children : <Navigate to="/login" replace />;
 };
 
 export {
     PrivateRoute,
-    ProtectedRoute
 }
